7bd44bad2a57ece357952d7e6abb875364e7a26d
Paulo Henrif <paulohenriquef931@gmail.com>
2024-08-25 12:28:28 -0300

    aaa

--- ponto/static/scripts/aprov.js
+++ ponto/static/scripts/aprov.js
@@ -4,14 +4,12 @@ const matricula = document.getElementById('digiteMatricula').value;
 const livre = document.getElementById('livre');
 const hora = document.getElementById('hourParagraph');
 const pessoa = document.getElementById('person_badge');
-const bookmark = document.getElementById('bookmark');
 
     if (matricula.length < 5) {
 
-        livre.style.backgroundColor = '#FFFFFF';
-        hora.style.backgroundColor = '#FFFFFF';
-        pessoa.style.backgroundColor = '#FFFFFF';
-        bookmark.style.backgroundColor = '#FFFFFF';
+        livre.style.backgroundColor = '#8B0000';
+        hora.style.color = '#8B0000';
+        pessoa.style.filter = 'hue-rotate(-90deg) brightness(70%)';
         event.preventDefault();
     }
 }
