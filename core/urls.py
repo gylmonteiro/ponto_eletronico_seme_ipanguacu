7f67308ff769928fea496cbc21c31ec2a5552b82
Gyl Monteiro <gyl.tecno.especialista@gmail.com>
2024-08-28 10:24:59 -0300

    atualizada a branch livre

--- core/urls.py
+++ core/urls.py
@@ -1,8 +1,9 @@
 from django.contrib import admin
 from django.urls import path, include
-from ponto.views import livre
+from ponto.views import livre, entrada
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', livre),
+    path('entrada', entrada, name='entrada')
 ]
--- ponto/views.py
+++ ponto/views.py
@@ -3,3 +3,9 @@ from django.shortcuts import render
 def livre(request):
     return render(request, "livre.html")
 
+def entrada(request):
+
+    if request.method == "POST":
+        matricula = request.POST.get("matricula")
+        print(matricula)
+    return render(request, "entrada.html")
