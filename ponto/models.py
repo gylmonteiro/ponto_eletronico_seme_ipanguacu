e55c89e155405ec1aa61b430d3d2a9f7b2b57d83
Endrew <endrew.developer@gmail.com>
2024-08-30 16:35:43 -0300

    Criador o modelo

--- ponto/models.py
+++ ponto/models.py
@@ -1,2 +1,16 @@
 from django.db import models
+from colaboradores.models import Colaborador
 
+class Entrada(models.Model):
+    COLABORADOR = models.ForeignKey(Colaborador, verbose_name='Colaborador')
+    DATA_HORA   = models.DateTimeField(verbose_name='Data e hora')
+
+    def __str__(self):
+        return self.DATA_HORA
+
+class Saida(models.Model):
+    COLABORADOR = models.ForeignKey(Colaborador, verbose_name='Colaborador')
+    DATA_HORA   = models.DateTimeField(verbose_name='Data e hora')
+
+    def __str__(self):
+        return self.DATA_HORA
