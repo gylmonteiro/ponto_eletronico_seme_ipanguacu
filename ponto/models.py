27989e84565967da4b9962e39d7e857d3ca6a2e2
Endrew <endrew.developer@gmail.com>
2024-08-30 17:01:50 -0300

    Mudança código

--- ponto/models.py
+++ ponto/models.py
@@ -2,14 +2,14 @@ from django.db import models
 from colaboradores.models import Colaborador
 
 class Entrada(models.Model):
-    COLABORADOR = models.ForeignKey(Colaborador, verbose_name='Colaborador')
+    COLABORADOR = models.ForeignKey(Colaborador, on_delete=models.CASCADE, verbose_name='Colaborador')
     DATA_HORA   = models.DateTimeField(verbose_name='Data e hora')
 
     def __str__(self):
         return self.DATA_HORA
 
 class Saida(models.Model):
-    COLABORADOR = models.ForeignKey(Colaborador, verbose_name='Colaborador')
+    COLABORADOR = models.ForeignKey(Colaborador, on_delete=models.CASCADE, verbose_name='Colaborador')
     DATA_HORA   = models.DateTimeField(verbose_name='Data e hora')
 
     def __str__(self):
